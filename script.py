aca7d6438b028c9ee60a53bdc92489828b1fb112
beemyanathip <147927894+beemyanathip@users.noreply.github.com>
2023-11-01 19:31:57 +0700

    script.py
    try to test committing the code

--- script.py
+++ script.py
@@ -1,34 +1,46 @@
 import requests
-security_point = 0
-url = 'http://testphp.vulnweb.com/'
-response = requests.get(url,timeout=5)
-headers = response.headers
-key,value = [],[]
-geninfo = []
-for k,v in headers.items():
-    key.append(k)
-    value.append(v)
-if "Server" in key:
-    if  any(map(str.isdigit, headers["Server"])):
-        geninfo.append(headers['Server'])
-if "X-Powered-By" in key:
-    geninfo.append(headers['X-Powered-By'])
-headers = '''X-Content-Type-Options
-Content-Security-Policy
-Strict-Transport-Security
-X-Frame-Options
-Referrer-Policy'''.split()
-missing = []
-print(f"#{'-'*50}")
-for header in headers:
-    if header not in key:
-        missing.append(header)
-if len(headers)>0:
-    print("The following security header are missing:")
-    for m in missing:
-        print(f" - {m}")
-else:   print("There no missing HTTP security header.\n Good job!")
-if len(geninfo)>0:
-    print("The attacker can gain the following information from HTTP response:")
-    for g in geninfo:
-        print(f" - {g}")
+import optparse
+
+def get_url_arguments():
+    parser = optparse.OptionParser()
+    parser.add_option('-u', '--url', dest='url', help='URL to check')
+    (options, args) = parser.parse_args()
+    return options.url
+
+url = get_url_arguments()
+if url is None:
+    print("Please provide a URL using the -u option.")
+else:
+    security_point = 0
+    response = requests.get(url, timeout=5)
+    headers = response.headers
+    key, value = [], []
+    geninfo = []
+    for k, v in headers.items():
+        key.append(k)
+        value.append(v)
+    if "Server" in key:
+        if any(map(str.isdigit, headers["Server"])):
+            geninfo.append(headers['Server'])
+    if "X-Powered-By" in key:
+        geninfo.append(headers['X-Powered-By'])
+    headers = '''X-Content-Type-Options
+    Content-Security-Policy
+    Strict-Transport-Security
+    X-Frame-Options
+    Referrer-Policy'''.split()
+    missing = []
+    print(f"#{'-' * 50}")
+    for header in headers:
+        if header not in key:
+            missing.append(header)
+    if len(headers) > 0:
+        print("The following security headers are missing:")
+        for m in missing:
+            print(f" - {m}")
+    else:
+        print("There are no missing HTTP security headers.\n Good job!")
+    if len(geninfo) > 0:
+        print("The attacker can gain the following information from the HTTP response:")
+        for g in geninfo:
+            print(f" - {g}") #teest
